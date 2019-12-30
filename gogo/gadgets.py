8add49504cb33c6190c6c57924d09a1b8768c218
Francis Whitesell <francis.whitesell@earnest.com>
2019-12-30 14:45:33 -0800

    removed extraneous code from gadgets.py

--- gogo/gadgets.py
+++ gogo/gadgets.py
@@ -2,9 +2,3 @@ def init_gadget(gobase):
     gobase.register_start()
     gobase.register_stop()
     gobase.register_clean()
-
-    version_subparser.add_argument('asset',
-                                   choices=['all', 'package-json', 'image'],
-                                   default='all',
-                                   nargs='?',
-                                   help='version assets of the service')
