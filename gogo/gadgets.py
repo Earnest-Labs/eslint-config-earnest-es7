1e5c940b383a5747c5adbde792e9402d1215b4af
Francis Whitesell <francis.whitesell@earnest.com>
2019-12-30 15:25:03 -0800

    registered publishing gadget

--- gogo/gadgets.py
+++ gogo/gadgets.py
@@ -2,3 +2,6 @@ def init_gadget(gobase):
     gobase.register_start()
     gobase.register_stop()
     gobase.register_clean()
+
+    gobase.register_custom_child('publish', 'publish the image for this library',
+                         f"npm install autopublish && ./node_modules/.bin/autopublish . .")
