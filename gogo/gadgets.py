f399b9f23e1dd638cfd6a3f0c874f3feb4ec948a
Francis Whitesell <francis.whitesell@earnest.com>
2020-01-02 09:26:31 -0800

    removed application start/stop defaults from gadgets.py since this is a library, and refactored the ./script/* indirection out.

--- gogo/gadgets.py
+++ gogo/gadgets.py
@@ -1,8 +1,6 @@
 import os
 
 def init_gadget(gobase):
-    gobase.register_start()
-    gobase.register_stop()
     gobase.register_clean()
 
     gobase.register_custom_child('publish', 'publish the image for this library',
--- scripts/ci/_common.sh
+++
@@ -1,9 +0,0 @@
-  #!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-function prep_for_ci {
-  : ${JENKINS_HOME?These scripts are designed to be run within CI. Aborting in a cowardly fashion.}
-}
-
-[ -n "${SKIP_CI_PREP-}" ] || prep_for_ci
