ac2e4c6716c22d52a5d560e5f03b97d9bd7366c3
Francis Whitesell <francis.whitesell@earnest.com>
2020-01-07 11:40:16 -0800

    deleted files that did not exist as of dec 2019

--- gogo/gadgets.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def init_gadget(gobase):
-    gobase.register_clean()
-
-    gobase.register_custom_child('publish', 'publish the image for this library',
-                         f"docker build --build-arg NPM_TOKEN=${os.getenv('NPM_TOKEN')} .")
